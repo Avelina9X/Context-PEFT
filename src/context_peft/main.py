b16b56501a3e614cf9e542b66a763d7f3d616685
Avelina <lhk3@st-andrews.ac.uk>
2025-06-26 20:26:39 +0100

    added microbatch multiplier for different devices

--- src/context_peft/main.py
+++ src/context_peft/main.py
@@ -7,11 +7,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument( '--config', required=True, type=str )
     parser.add_argument( '--seed_offset', required=False, type=int )
+    parser.add_argument( '--micro_batch_mult', required=False, type=int, default=1 )
     args = parser.parse_args()
 
     with open( args.config, 'r', encoding='utf-8' ) as f:
         config = yaml.safe_load( f )
         assert isinstance( config, dict )
+
+    config[ 'micro_batch_size' ] *= args.micro_batch_mult
     
     trainer_config = TrainerConfig(
         **config
